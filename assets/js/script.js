d762be2f1eff8fa6bd5e152371c7874e8d4ffa57
Wellington Alves <alveswellingtonmoraes@hotmail.com>
2023-02-24 14:11:37 -0300

    Relógio digital em Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const hours = document.getElementById('horas');
+const minutes = document.getElementById('minutos');
+const seconds = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+
+    let date = new Date();
+
+    let hora = date.getHours();
+    let minuto = date.getMinutes();
+    let segundo = date.getSeconds();
+
+    const horas = hora < 10 ? `0${hora}` : hora;
+    const minutos = minuto < 10 ? `0${minuto}` : minuto;
+    const segundos = segundo < 10 ? `0${segundo}` : segundo;
+
+    hours.textContent = horas;
+    minutes.textContent = minutos;
+    seconds.textContent = segundos; 
+
+    // hours.innerText = horas;
+    // minutes.innerText = minutos;
+    // seconds.innerText = segundos;
+});
